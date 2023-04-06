10a49683c282ef3b0dc14ecb4c72cd5ee31d5db1
Dilkhan04 <dilkhan.04.e@gmail.com>
2023-04-06 12:18:39 +0600

    skeleton

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import style from "./css/footer.module.scss";
+
+export default function Footer() {
+  return <div className={style.Footer}>Footer</div>;
+}
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import style from "./css/header.module.scss";
+
+export default function Header() {
+  return <div className={style.Header}>Header</div>;
+}
